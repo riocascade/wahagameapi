ae6c25feca37ffd527ad582d4f42edc4b4f00401
riocascade <46962155+riocascade@users.noreply.github.com>
2025-11-25 21:22:44 +0700

    Add files via upload

---
+++ netlify/functions/notion.js
@@ -0,0 +1,34 @@
+exports.handler = async (event, context) => {
+  try {
+    const response = await fetch(
+      "https://api.notion.com/v1/databases/2b2fef65a2c18086a3fffc774b6e7aeb/query",
+      {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Notion-Version": "2022-06-28",
+          "Authorization": "Bearer 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" // SOP: token di sini
+        },
+        body: event.body
+      }
+    );
+
+    const data = await response.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Content-Type": "application/json"
+      },
+      body: data
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
+
