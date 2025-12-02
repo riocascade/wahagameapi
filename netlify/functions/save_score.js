7fb5c7ea7463ca38bf67fcf3725c143d4085043d
riocascade <46962155+riocascade@users.noreply.github.com>
2025-12-02 23:52:07 +0700

    Rename phone filter to phone_number

--- netlify/functions/save_score.js
+++ netlify/functions/save_score.js
@@ -19,7 +19,7 @@ exports.handler = async (event, context) => {
     const queryPayload = {
       filter: {
         property: "Mobile Phone",
-        phone: { equals: phone }
+        phone_number: { equals: phone }
       }
     };
 
