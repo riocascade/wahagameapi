aecdad4606b10546a0bd4695ec49cdb030f4a61d
riocascade <46962155+riocascade@users.noreply.github.com>
2025-12-03 21:50:52 +0700

    Implement leaderboard fetching in get_leaderboard.js

---
+++ netlify/functions/get_leaderboard.js
@@ -0,0 +1,55 @@
+exports.handler = async (event, context) => {
+  console.log("TOKEN IN NETLIFY:", process.env.NOTION_TOKEN);
+
+  try {
+    const response = await fetch(
+      `https://api.notion.com/v1/databases/${process.env.NOTION_DATABASE_ID}/query`,
+      {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Notion-Version": "2022-06-28",
+          "Authorization": `Bearer ${process.env.NOTION_TOKEN}`,
+          "User-Agent": "WahahaGame/1.0"
+        },
+        body: JSON.stringify({
+          page_size: 20,
+          sorts: [
+            {
+              "property": "score",
+              "direction": "descending"
+            }
+          ]
+        })
+      }
+    );
+
+    const result = await response.json();
+
+    const leaderboard = result.results.map(item => {
+      const props = item.properties;
+      return {
+		  score: props.Score?.number || 0,
+		  mobilePhone: props["Mobile Phone"]?.phone_number || "",
+		  firstName: props["First Name"]?.rich_text?.[0]?.plain_text || "",
+		  lastName: props["Last Name"]?.rich_text?.[0]?.plain_text || "",
+		};
+    });
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(leaderboard)
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
